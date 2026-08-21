736cfb693d2cb54f826fee1ce892d097f048be9f
Aman Singh <7347amansingh@gmail.com>
2026-08-21 19:39:06 +0530

    Refactor BlockPool comments and fix typos
    Updated comments for clarity and corrected spelling errors in error messages.

--- engine/memory.py
+++ engine/memory.py
@@ -1,11 +1,11 @@
-# BlockPool of Paged kv
+
 class BlockPool:
   def __init__(self, total_blocks : int, block_size : int, num_layers : int, num_heads : int , head_dim : int , device):
     self.total_blocks = total_blocks
     self.block_size = block_size
     self.device = device
 
-    self.free_blocks = list(range(total_blocks))
+    self.free_blocks = list(range(total_blocks))   # free_blocks is simply a list of which blocks are currently available with an id.
 
 
     # Kv storage
@@ -30,14 +30,17 @@ class BlockPool:
         dtype= torch.float16
     )
 
-  def allocate(self):
+  def allocate(self):   # allocate do not allocate gpu memory it just says Give this request ownership of block 4. if initialy block size is 5
     if not self.free_blocks:
-      raise RuntimeError("Out of kv memeory blocks")
+      raise RuntimeError("Out of kv memeory Block")
 
-    block_id = self.free_blocks.pop()
+    block_id = self.free_blocks.pop()  # here we are removing it because block containes a request kv cache
     print(f'[Alloc] Block {block_id}')
     return block_id
 
   def free(self, block_id):
-    self.free_blocks.append(block_id)
-    print(f'[Free] Block {block_id}')
+    self.keys[:, block_id, :, :, :] = 0.0
+    self.values[:, block_id, :, :, :] = 0.0
+    self.free_blocks.append(block_id)  # here we are again appending it because the request has been completed so making it ready for next request
+    print(f'[Free] Block {block_id}')  # even after completion of request A the same block can hold the previous kv cache but once the block is allocated to
+                                       # request B it overrides the previous kv cache
